36f16265ef16ca3841623fcc2a0bb69e72526837
mixcropro <62208129+mixcropro@users.noreply.github.com>
2020-03-19 17:34:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import *
+from trening import *
 
-Trening = {
-     "Noge":[
-       """      Čučanj: 10 ponavljanja, 3 seta, odmor: 90 sec \n
-        Leg Extension: 10 ponavljanja, 3 seta, odmor: 90 sec \n
-        Leg Press: 12 ponavljanja, 3 seta, odmor: 90 sec \n
-        Leg Curl: 12 ponavljanja,3 seta, odmor: 70 sec \n 
-        DB Lunges: 14 ponavljanja, 3 seta, odmor: 70 sec"""
-],
-    "Sisice":[
-       """      Incline BB press: 8 ponavljanja, 3 seta, odmor: 90 sec\n
-       Flat DB press: 10 ponavljanja, 3 seta, odmor 90sec \n
-       Seated DB shoulder press: 8 ponavljanja, 3 seta, odmor: 90 sec\n
-       Cable fly: 10 ponavljanja, 3 seta, odmor: 90 sec\n
-       [Overhead tricep DB extension: 8 ponavljanja, 3 seta, odmor: 80 sec + 
-       Seated DB lateral raise: 12 ponavljanja, 3 seta, odmor: 80 sec]\n
-       Lying EZ skull crushers: 10 ponavljanja, 3 seta, odmor: 80 sec\n
-       Tricep cable pressdown: 12 ponavljanja, 3 seta, odmor: 80 sec"""
-  ],
-    "Leda":[
-        """     Mrtva dizanja: 10 ponavljanja, 3 seta, odmor: 90 sec\n
-        Chin up (neutral grip): 6-10 ponavljanja, 3 serije, odmor: 120sec\n 
-        Lat pulldown: 10 ponavljanja, 3 serije, odmor 90sec\n
-        One arm DB row: 10 ponavljanja, 3 serije, odmor 70sec\n
-        Standing BB curls:  8 ponavljanja, 3 serije, odmor 80sec\n
-        Standing BB shrugs: 8 ponavljanja, 3 serije, odmor 70sec\n
-        [Incline DB hammer curls: 10 ponavljanja, 3 serije, odmor 90sec + 
-        DB rear delt fly: 12 ponavljanja, 3 serije, odmor 90sec]\n
-        Single arm DB preacher curls: 10 ponavljanja, 3 serije, odmor 70sec"""
-    ]
-}
+
+def hide_me(self, event):
+    print('hide me')
+    event.widget.place_forget()
+
 def callback():
     label = Label(window, text=Trening.get("Noge"), font=("Arial Bold", 13), foreground = 'blue')
     label.pack()
@@ -40,7 +16,6 @@ def callback():
 def callback1():
     label = Label(window, text=Trening.get("Sisice"), font=("Arial Bold", 13), foreground = 'purple')
     label.pack()
-
 
 def callback2():
     label = Label(window, text=Trening.get("Leda"), font=("Arial Bold", 13), foreground = 'orange')
@@ -57,10 +32,14 @@ style.configure('TButton', font =
 
 b = ttk.Button(window, text="Noge")
 b.config(command=callback)
+
 b.pack()
+
+
 b1 = ttk.Button(window, text="Prsa")
 b1.config(command=callback1)
 b1.pack()
+
 b2 = ttk.Button(window, text="Leđa")
 b2.config(command=callback2)
 b2.pack()
---
+++ trening.py
@@ -0,0 +1,30 @@
+Trening = {
+     "Noge":[
+       """      Čučanj: 10 ponavljanja, 3 seta, odmor: 90 sec \n
+        Leg Extension: 10 ponavljanja, 3 seta, odmor: 90 sec \n
+        Leg Press: 12 ponavljanja, 3 seta, odmor: 90 sec \n
+        Leg Curl: 12 ponavljanja,3 seta, odmor: 70 sec \n 
+        DB Lunges: 14 ponavljanja, 3 seta, odmor: 70 sec"""
+],
+    "Sisice":[
+       """      Incline BB press: 8 ponavljanja, 3 seta, odmor: 90 sec\n
+       Flat DB press: 10 ponavljanja, 3 seta, odmor 90sec \n
+       Seated DB shoulder press: 8 ponavljanja, 3 seta, odmor: 90 sec\n
+       Cable fly: 10 ponavljanja, 3 seta, odmor: 90 sec\n
+       [Overhead tricep DB extension: 8 ponavljanja, 3 seta, odmor: 80 sec + 
+       Seated DB lateral raise: 12 ponavljanja, 3 seta, odmor: 80 sec]\n
+       Lying EZ skull crushers: 10 ponavljanja, 3 seta, odmor: 80 sec\n
+       Tricep cable pressdown: 12 ponavljanja, 3 seta, odmor: 80 sec"""
+  ],
+    "Leda":[
+        """     Mrtva dizanja: 10 ponavljanja, 3 seta, odmor: 90 sec\n
+        Chin up (neutral grip): 6-10 ponavljanja, 3 serije, odmor: 120sec\n 
+        Lat pulldown: 10 ponavljanja, 3 serije, odmor 90sec\n
+        One arm DB row: 10 ponavljanja, 3 serije, odmor 70sec\n
+        Standing BB curls:  8 ponavljanja, 3 serije, odmor 80sec\n
+        Standing BB shrugs: 8 ponavljanja, 3 serije, odmor 70sec\n
+        [Incline DB hammer curls: 10 ponavljanja, 3 serije, odmor 90sec + 
+        DB rear delt fly: 12 ponavljanja, 3 serije, odmor 90sec]\n
+        Single arm DB preacher curls: 10 ponavljanja, 3 serije, odmor 70sec"""
+    ]
+}
